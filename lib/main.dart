fb7eb9eb84994aa314d6ddc25552f3f95e8c6d22
Pablo Rodríguez <prodrigu75@gmail.com>
2024-07-15 19:39:58 -0400

    Before GUI

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app/app.dart';
+import 'package:pizza_app/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  await Firebase.initializeApp();
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, World!!'),
-        ),
-      ),
-    );
-  }
+  Bloc.observer = SimpleBlocObserver();
+
+  runApp(MyApp(FirebaseUserRepo()));
 }
